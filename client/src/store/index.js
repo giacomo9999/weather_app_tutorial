6cc9ad9b21b6dd1e2c486aad116ec000de386649
James Gary <gmarchesi888@gmail.com>
2018-12-16 11:19:48 -0500

    Fixed (apparently) issues with Redux dependency.

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,15 +1,14 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import createSagaMiddleware from 'redux-saga';
-import rootReducer from './reducers/index';
-import rootSaga from './sagas/index';
+import { applyMiddleware, createStore } from "redux";
+import createSagaMiddleware from "redux-saga";
+import rootReducer from "./reducers/index";
+import rootSaga from "./sagas/index";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const sagaMiddleware = createSagaMiddleware();
 
-const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-
 const store = createStore(
   rootReducer,
-  compose(applyMiddleware(sagaMiddleware), reduxDevTools)
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
 );
 
 sagaMiddleware.run(rootSaga);
--- server/routes/api/SearchLocations.js
+++ server/routes/api/SearchLocations.js
@@ -1,11 +1,30 @@
-const fetch = require('node-fetch');
-const generateWebAppURL = require('server/utils').generateWebAppURL;
+const fetch = require("node-fetch");
+const generateWebAppURL = require("server/utils").generateWebAppURL;
 
-module.exports = (app) => {
+module.exports = app => {
+  let zipcode;
 
-  app.post('/search-location-weather', (req, res) => {
-    const requestBody = req.body;
-    const apiUrl = generateWebAppURL(requestBody.locationType, requestBody.locationData);
+  app.post("/search-location", (req, res) => {
+    zipcode = req.body.zipcode;
+    if (!zipcode || zipcode.length < 5 || zipcode.length > 5) {
+      res.redirect("/error");
+    } else {
+      res.redirect("/current-weather");
+    }
+  });
+
+  app.get("/search-location-weather", (req, res) => {
+    //build api URL with user zip
+    const baseUrl = "http://api.openweathermap.org/data/2.5/weather?zip=";
+    //ENTER YOUR API KEY HERE (make sure to no include < >)
+    const apiId = "&appid=da7e909781b2dc477e5c92f6ad1977e0&units=imperial";
+
+    const userLocation = (url1, url2, zipcode) => {
+      let newUrl = url1 + zipcode + url2;
+      return newUrl;
+    };
+
+    const apiUrl = userLocation(baseUrl, apiId, zipcode);
 
     fetch(apiUrl)
       .then(res => res.json())
@@ -13,7 +32,24 @@ module.exports = (app) => {
         res.send({ data });
       })
       .catch(err => {
-        res.redirect('/error');
+        res.redirect("/error");
+      });
+  });
+
+  app.post("/search-location-weather", (req, res) => {
+    const requestBody = req.body;
+    const apiUrl = generateWebAppURL(
+      requestBody.locationType,
+      requestBody.locationData
+    );
+
+    fetch(apiUrl)
+      .then(res => res.json())
+      .then(data => {
+        res.send({ data });
+      })
+      .catch(err => {
+        res.redirect("/error");
       });
   });
 };
